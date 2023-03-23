9b3a137a668ac2bae9591ac7fd4b80ee9bff1828
MONTOJO Juan Jose <juan-jose.valles@ext.soprasteria.com>
2023-03-23 18:59:47 +0100

    Proyecto2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import data from '../assets/products.json';
+
+interface DATA {
+  product: String;
+  price: Number;
+  currency: String;
+  rating: Number;
+  description: String;
+  similarProducts: {};
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ProyectoInicial';
+  data: DATA[] = data;
+  buttonClick() {
+    console.log(data);
+  }
 }
