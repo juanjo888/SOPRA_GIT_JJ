aa7268394bb054d13706198a5b671270f0b65f92
Xavier García <100375613+elxaavoo@users.noreply.github.com>
2023-04-13 08:18:42 +0200

    cambios de xavi

--- src/app/modules/form/general/general.component.ts
+++ src/app/modules/form/general/general.component.ts
@@ -1,31 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Producto } from 'src/app/interfaces/producto';
+import { DataserviceService } from 'src/app/services/dataservice.service';
 
 
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.scss']
+  styleUrls: ['./general.component.scss'],
 })
-export class GeneralComponent {
-userForm: FormGroup = this.formBuilder.group({
-  product: [null, Validators.required],
-  price: [null, Validators.required],
-  description: [null, Validators.required]
-});
+export class GeneralComponent implements OnInit {
+  userForm: FormGroup = this.formBuilder.group({
+    product: [null, Validators.required],
+    price: [null, Validators.required],
+    description: [null, Validators.required],
+  });
 
-formValue: any;
+  formValue: any;
 
-constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: DataserviceService
+  ) {}
 
-sendUserInfo() {
-  this.formValue = this.userForm.value;
-}
+  productos: Producto[] = []
 
-validateField(field: string): boolean {
-  return (
-    this.userForm.controls[field].invalid &&
-    this.userForm.controls[field].touched
-  );
-}
+  ngOnInit() {
+    this.dataService.$products.subscribe({
+      next: (response) => {
+        this.productos = response;
+      },
+      error: () => {},
+      complete: () => {
+        console.log('Terminado');
+      },
+    });
+  }
+
+  sendUserInfo() {
+    this.formValue = this.userForm.value;
+  }
+
+  validateField(field: string): boolean {
+    return (
+      this.userForm.controls[field].invalid &&
+      this.userForm.controls[field].touched
+    );
+  }
 }
